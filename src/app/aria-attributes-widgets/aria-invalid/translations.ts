7ec1adca5dadc4c15b25ec26517875ce28c92b29
Marco <signorello.marco@gmail.com>
2025-03-31 22:55:22 +0200

    updated error of aria-invalid example

--- src/app/aria-attributes-widgets/aria-invalid/translations.ts
+++ src/app/aria-attributes-widgets/aria-invalid/translations.ts
@@ -1,10 +1,10 @@
 export const translations = {
   en: {
-    emailError: "Please enter a valid email address",
+    emailError: "Please enter a valid email address with the format 'test@email.com'",
     emailAddressLabel: "Email Address",
   },
   es: {
-    emailError: "Por favor, ingrese una dirección de correo electrónico válida",
+    emailError: "Por favor, ingrese una dirección de correo electrónico válida con el formato 'prueba@email.com'",
     emailAddressLabel: "Correo electrónico",
   },
 };
